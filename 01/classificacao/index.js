bf2c3ac24628802c06debee8dea7710051333b29
Alexandre Servian <alexandreservian@gmail.com>
2017-10-27 18:12:47 -0200

    adding more variables in the algorithm "classification"

--- 01/classificacao/index.js
+++ 01/classificacao/index.js
@@ -27,4 +27,17 @@ model.train(data, marks);
 
 const result = model.predict(test);
 
-console.log(result)
+const differences = result.map((value, index) => value - marks_test[index]);
+const hits = differences.filter(value => value===0);
+const totalHits = hits.length;
+const totalElementsTest = test.length;
+const hitRate = 100 * (totalHits / totalElementsTest);
+
+const message = `
+                  Result: ${result},
+                  Hits: ${hits},
+                  Total Hits: ${totalHits},
+                  Hit Rate: ${hitRate}%
+                `;
+                
+console.log(message)
